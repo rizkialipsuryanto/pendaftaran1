ea5ab2cd0bffff1894bceec7fca061616cc20277
rizkialipsuryanto <rizkialip8@gmail.com>
2020-03-01 21:28:29 +0700

    menambah xml untuk button red, menambah library untuk qrcode dan membuat hlaman home untuk qrcode

--- app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/fragment/HomeFragment.java
+++ app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.pendaftaran1.rsudajibarang.pendaftaran1.fragment;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import com.pendaftaran1.rsudajibarang.pendaftaran1.R;
 
@@ -15,6 +24,13 @@ import com.pendaftaran1.rsudajibarang.pendaftaran1.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    public Button barcode;
+    public ImageView image;
+
+    String teksbarcode;
+
+    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
 
 
     public HomeFragment() {
@@ -26,7 +42,43 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        barcode = (Button)view.findViewById(R.id.barcodeteks);
+        image = (ImageView)view.findViewById(R.id.imageview);
+
+        barcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String greeting = "Hello world!";
+//                teksbarcode = teks.getText().toString();
+                try
+                {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(greeting, BarcodeFormat.QR_CODE, 300,300);
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    Bitmap bitmap = encoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return view;
     }
+
+//    public void onClick(View v) {
+//        String greeting = "Hello world!";
+////        teksbarcode = aaaaa;
+//        try                {
+//            BitMatrix bitMatrix = multiFormatWriter.encode(greeting, BarcodeFormat.QR_CODE, 300,300);
+//            BarcodeEncoder encoder = new BarcodeEncoder();
+//            Bitmap bitmap = encoder.createBitmap(bitMatrix);
+//            image.setImageBitmap(bitmap);
+//
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
