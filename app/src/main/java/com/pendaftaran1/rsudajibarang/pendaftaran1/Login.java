fe0d0d725c4eaca1f0518ee60bade398f4214bd3
rizkialipsuryanto <rizkialip8@gmail.com>
2020-03-05 14:29:03 +0700

    jajal registrasi masih gagal

--- app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/Login.java
+++ app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/Login.java
@@ -93,24 +93,22 @@ public class Login extends AppCompatActivity {
     }
 
 
-    private void login(){
+    private void login() {
 
         // REST LOGIN ------------------------------------------------------------------
         RestServices restServices = ServiceGenerator.build().create(RestServices.class);
-        Call login = restServices.Login(usernamea.getText().toString(),passwordd.getText().toString());
+        Call login = restServices.Login(usernamea.getText().toString(), passwordd.getText().toString());
         login.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
 
-                try{
+                try {
                     JSONObject jo = new JSONObject(response.body().toString());
-
                     JSONObject rrrr = jo.getJSONObject("response");
-
                     String b = rrrr.getString("token");
                     Log.d("OBJEK", b);
-                    if(b.length()>0){
-                        Toast.makeText(getApplicationContext(),"Berhasil login", Toast.LENGTH_LONG).show();
+                    if (b.length() > 0) {
+                        Toast.makeText(getApplicationContext(), "Berhasil login", Toast.LENGTH_LONG).show();
                         // menyimpan login ke session
                         SharedPreferences.Editor editor = sharedpreferences.edit();
                         editor.putBoolean(session_status, true);
@@ -125,17 +123,16 @@ public class Login extends AppCompatActivity {
 //
 //                        Intent i = new Intent(Login.this, indexActivity.class); //MainActivity adalah aktivity awal ,praktikum1Activity activity yang akan di tuju
 //                        startActivity(i);
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(),"Login gagal", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Login gagal", Toast.LENGTH_LONG).show();
                         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Login.this);
                         alertDialogBuilder.setMessage("Gagal login");
                     }
 //                    Intent i = new Intent(Login.this, indexActivity.class); //MainActivity adalah aktivity awal ,praktikum1Activity activity yang akan di tuju
 //                    startActivity(i);
-                }catch(Exception e){
+                } catch (Exception e) {
 //                    Log.d("OBJEK", e.getMessage());
-                    Toast.makeText(getApplicationContext(),"Login gagal", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Login gagal", Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -145,74 +142,6 @@ public class Login extends AppCompatActivity {
                 Log.d("CEK", t.getMessage());
             }
         });
-
-        // ---------------------------------------------------------------------------------
-
-
-// REST BACA LIST USER  ------------------------------------------------------------------
-//        Call  abc =  restServices.listUsers();
-//
-//        abc.enqueue(new Callback() {
-//            @Override
-//            public void onResponse(Call call, Response response) {
-//                Log.d("CEK", response.toString());
-//                Log.d("CEK", response.body().toString());
-//            }
-//
-//            @Override
-//            public void onFailure(Call call, Throwable t) {
-//                Log.d("CEK", t.getMessage());
-//            }
-//        });
-// ---------------------------------------------------------------------------------
-
-//        mLogin login = new mLogin("ariguswahyu.id@gmail.com", "54321");
-//        Call<mUser> call = userClient.login(login);
-//        call.enqueue(new Callback<mUser>() {
-//            @Override
-//            public void onResponse(Call<mUser> call, Response<mUser> response) {
-//                if(response.isSuccessful()){
-//                    Toast.makeText(Login.this, response.body().getToken(), Toast.LENGTH_SHORT).show();
-//                    token = response.body().getToken();
-//                }
-//                else
-//                {
-//                    Toast.makeText(Login.this, "login salah :", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<mUser> call, Throwable t) {
-//                Toast.makeText(Login.this, "error :", Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
-
-//    private void getSecret(){
-//        Call<ResponseBody> call = userClient.getSecret(token);
-//        call.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                if (response.isSuccessful()){
-//                    try {
-//                        Toast.makeText(Login.this, response.body().string(), Toast.LENGTH_SHORT).show();
-//                    }
-//                    catch (IOException e){
-//                        e.printStackTrace();
-//                    }
-//                }
-//                else
-//                {
-//                    Toast.makeText(Login.this, "token is not :", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                Toast.makeText(Login.this, "error :", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-
 }
 
--- app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/daftarakun.java
+++ app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/daftarakun.java
@@ -2,13 +2,120 @@ package com.pendaftaran1.rsudajibarang.pendaftaran1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.pendaftaran1.rsudajibarang.pendaftaran1.app.AppController;
+import com.pendaftaran1.rsudajibarang.pendaftaran1.constant.Base;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class daftarakun extends AppCompatActivity {
 
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private static final String TAG = indexActivity.class.getSimpleName();
+    private String url_insert = Base.REST_BASE_URL + "auth/registration";
+    int success;
+    String firstnames, lastnames, emails,passwords;
+    EditText firstnamee, lastnamee,emaile, passworde;
+    Button btn_lanjut;
+
+    String tag_json_obj = "json_obj_req";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftarakun);
+
+        firstnamee = (EditText) findViewById(R.id.firstname);
+        lastnamee = (EditText) findViewById(R.id.lastname);
+        emaile = (EditText) findViewById(R.id.email);
+        passworde = (EditText) findViewById(R.id.password);
+        btn_lanjut = (Button) findViewById(R.id.daftarsimpan);
+
+        btn_lanjut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpan();
+                Intent intent = new Intent(daftarakun.this, Login.class);
+                finish();
+                startActivity(intent);
+
+
+            }
+        });
+    }
+
+    private void simpan() {
+        String url;
+        url = url_insert;
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response: " + response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+
+                    // Cek error node pada json
+                    if (success == 1) {
+                        Log.d("Add/update", jObj.toString());
+                        Toast.makeText(daftarakun.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Toast.makeText(daftarakun.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(daftarakun.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Posting parameters ke post url
+                Map<String, String> params = new HashMap<String, String>();
+                // jika id kosong maka simpan, jika id ada nilainya maka update
+
+                params.put("firstname", firstnamee.getText().toString());
+                params.put("lastname", lastnamee.getText().toString());
+                params.put("email", emaile.getText().toString());
+                params.put("password", passworde.getText().toString().trim());
+
+                return params;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
     }
 }
