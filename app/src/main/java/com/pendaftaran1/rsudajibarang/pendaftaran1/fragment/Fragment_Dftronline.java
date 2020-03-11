af4e5be3e3199b941891340f7e8369433b62ebb1
lunart21 <lukmannurarifin21@gmail.com>
2020-03-11 22:32:21 +0700

    megubah type pasien baru / lama jadi spinner, radio button, menambahkan fragment daftar online

---
+++ app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/fragment/Fragment_Dftronline.java
@@ -0,0 +1,29 @@
+package com.pendaftaran1.rsudajibarang.pendaftaran1.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pendaftaran1.rsudajibarang.pendaftaran1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_Dftronline extends Fragment {
+
+    public Fragment_Dftronline() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__dftronline, container, false);
+    }
+}
