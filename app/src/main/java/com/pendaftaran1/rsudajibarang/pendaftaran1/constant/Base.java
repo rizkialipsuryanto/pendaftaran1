e032e4c5c412fc71ee465f17a091df51d2796b94
lunar21 <lukmannurarifin21@gmail.com>
2020-03-13 14:41:18 +0700

    Menambahkan Fragment Poli dan Riwayat beserta Recycler View, belum di desain. insyallah nanti malem

--- app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/constant/Base.java
+++ app/src/main/java/com/pendaftaran1/rsudajibarang/pendaftaran1/constant/Base.java
@@ -2,6 +2,6 @@ package com.pendaftaran1.rsudajibarang.pendaftaran1.constant;
 
 public class Base {
     public static final String REST_BASE_URL = "http://10.0.2.2/";
-    public static final String URL = "http://192.168.43.241/api.simrs.rsudajibarang/api/";
-    public static final String REST_URL = "http://192.168.43.241/";
+    public static final String URL = "http://192.168.1.161/api.simrs.rsudajibarang/api/";
+    public static final String REST_URL = "http://192.168.1.161/";
 }
